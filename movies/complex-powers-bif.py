948e20d7016678b7af9cb6173f81114c31c7a010
Gregory Propf <gpropf@gmail.com>
2018-01-13 14:58:08 -0800

    testing git ssh keys

--- movies/complex-powers-bif.py
+++ movies/complex-powers-bif.py
@@ -9,7 +9,7 @@ sys.path.append("../")
 import Spiderbrot
 
 
-## Git test - ignore
+## Git test - ignore 2
 
 turtle.setworldcoordinates(-3,-3,3,3)
 Spiderbrot.makeParallelMovie(0, 10000, 1000, "spbif4", 2)
